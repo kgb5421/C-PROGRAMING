f3e34246ba6f4790cd292a7e6f92d660a6a9af93
kgb5421 <hwangjaxx@gmail.com>
2024-09-29 15:44:47 +0900

    5주차 - 2

---
+++ 5주차 - 2.c
@@ -0,0 +1,16 @@
+/* 문자열 실습3 */
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char src[100];
+	char dest[100];
+	int result;
+	gets(src);
+	gets(dest);
+
+	result = strcmg(src, dest);
+	printf("result = %d\n", result);
+	return 0;
+}
